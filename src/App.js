430c015cb5f329058d99f1955675d7751e472cf9
matheusarthus <matheus.arthus@gmail.com>
2020-02-18 17:18:17 -0300

    chore: desafio finalizado

--- src/App.js
+++ src/App.js
@@ -1,20 +1,17 @@
 import React from 'react';
 
 import './App.css';
-import './Main.css';
 
 import Header from './components/Header';
 import PostList from './components/PostList'
 
 function App() {
   return (  
-    <div id="app">
+    <>
       <Header />
-
-      <main>
-        <PostList />
-      </main>
-    </div>
+       
+      <PostList />
+    </>
   );
 }
 
--- src/components/Header.js
+++ src/components/Header.js
@@ -1,15 +1,18 @@
 import React, { Component } from 'react';
 
-import './Header.css';
-
 import logo from '../assets/Facebook_f_logo_(2019).png';
 
 class Header extends Component {
   render() {
     return (
       <header>
-        <img src={logo}/>
-        <h1>Meu perfil</h1>
+        <nav>
+          <img src={logo}/>
+          <div>
+            <span>Meu perfil</span>
+            <i className="material-icons">account_circle</i>
+          </div>
+        </nav>
       </header>
     )
   }
